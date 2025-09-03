2ac4d43688d2bbd3b49db4e23783a339a7a32a6e
Samuel Marle <samzelmable@gmail.com>
2025-09-03 19:19:30 +0200

    Fix routing with HashRouter and update package.json

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { HashRouter as Router } from 'react-router-dom';
-import App from './App';
+import App from './App.jsx';   // explicitly reference App.jsx
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
